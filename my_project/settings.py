de59e0cbd59876fc72414a2cedcee849c3731f09
Fabio Williams-xavier <fabiobmk@gmail.com>
2024-11-12 14:03:37 +0000

    add heroku connection to the app

--- my_project/settings.py
+++ my_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-f%wq-3v7-+b(4cg4qnby%6v2tsz5816s0&@&_u7*usak@*#=%g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['8000-fabiowxavie-djangoproje-1brmvl9uu51.ws.codeinstitute-ide.net']
+ALLOWED_HOSTS = ['8000-fabiowxavie-djangoproje-1brmvl9uu51.ws.codeinstitute-ide.net','.herokuapp.com']
 
 
 # Application definition
